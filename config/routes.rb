bcbabecf33efc6996021e8e662fa2286a6d5a70d
yuichiro.nishimura <yuichiro_1988@knd.biglobe.ne.jp>
2018-12-11 10:39:27 +0000

    CRUD tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+  
+  resources :tasks
+
 end
+
+#Rails.application.routes.draw do
+  # CRUD
+  #get 'messages/:id', to: 'messages#show'
+  #post 'messages', to: 'messages#create'
+  #put 'messages/:id', to: 'messages#update'
+  #delete 'messages/:id', to: 'messages#destroy'
+
+  # index: show の補助ページ
+  #get 'messages', to: 'messages#index'
+
+  # new: 新規作成用のフォームページ
+  #get 'messages/new', to: 'messages#new'
+
+  # edit: 更新用のフォームページ
+  #get 'messages/:id/edit', to: 'messages#edit'
+#end
